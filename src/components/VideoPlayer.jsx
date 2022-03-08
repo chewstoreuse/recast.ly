a4dd5b7e0eb9d3f718908d2997411862028ec720
James <qjameso@berkeley.edu>
2022-03-07 21:15:15 -0800

    completed static site

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,13 +1,13 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
-  </div>
+  </div >
 );
 
 // PropTypes tell other developers what `props` a component expects
@@ -19,3 +19,6 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default VideoPlayer;
+
+
+//`https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`
